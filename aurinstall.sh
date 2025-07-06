506bedb0c38589f9f10a7984ca6e80ab37949d7d
Guilherme de Oliveira <coelhoposa@proton.me>
2025-07-06 16:47:13 -0300

    Add files via upload

---
+++ aurinstall.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+instalarAUR () {
+	echo -e "\nHá alguns pacotes que compõe o Nippybox que estão no AUR (Arch Linux User Repository), e para instalar eles é necessário o uso de um ajudante de AUR. O yay é o Ajudante de AUR que esse Script utiliza."
+
+	echo -e "\n## Instalando as Dependências do Yay..."
+	sudo pacman -S git go --noconfirm
+
+	echo -e "\n## Clonando o Repositório do Yay..."
+	git clone https://aur.archlinux.org/yay.git
+	cd yay
+
+	echo -e "\n## Instalando o Yay"
+	makepkg -si
+
+}
+
+instalarPacotesAUR () {
+	echo "## Instalando as Dependências Faltantes..."
+	sudo pacman -Syu betterlockscreen --noconfirm
+
+}
+
+instalarAUR
+instalarPacotesAUR
+
+
+
+
