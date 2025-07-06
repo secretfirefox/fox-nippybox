dc38a1dde30319e299384a622aeeb90839391d34
Guilherme de Oliveira <coelhoposa@proton.me>
2025-07-05 23:57:42 -0300

    Script modificado para ficar mais enxuto

--- install.sh
+++ install.sh
@@ -8,225 +8,21 @@ verificarDiretorios () {
 	mkdir -p $HOME/.themes/nippybox
 }
 
-verificarDependencias () {
-	echo -e "\n## Verificando Dependências..."
-	install=""
-	## Verificando se as Dependências estão instaladas
-
-	echo "=== DESKTOP ==="
-	## Openbox, xorg, obconf-qt
-	if [ -z "$(command -v openbox)" ]; then
-			echo "- Openbox: Não Instalado"
-			install+="openbox xorg obconf-qt archlinux-xdg-menu "
-		else
-			echo "- Openbox: OK"
-	fi
-
-	## Polybar
-	if [ -z "$(command -v polybar)" ]; then
-			echo "- Polybar: Não Instalado"
-			install+="polybar "
-		else
-			echo "- Polybar: OK"
-	fi
-
-	## Rofi
-	if [ -z "$(command -v rofi)" ]; then
-			echo "- Rofi: Não Instalado"
-			install+="rofi "
-		else
-			echo "- Rofi: OK"
-	fi
-
-	## Dunst
-	if [ -z "$(command -v dunst)" ]; then
-			echo "- Dunst: Não Instalado"
-			install+="libnotify dunst "
-		else
-			echo "- Dunst: OK"
-	fi
-
-	## Nitrogen
-	if [ -z "$(command -v nitrogen)" ]; then
-			echo "- Nitrogen: Não Instalado"
-			install+="nitrogen "
-		else
-			echo "- Nitrogen: OK"
-	fi
-
-	## Picom
-	if [ -z "$(command -v picom)" ]; then
-			echo "- Picom: Não Instalado"
-			install+="picom "
-		else
-			echo "- Picom: OK"
-	fi
-
-	## Plank
-	if [ -z "$(command -v plank)" ]; then
-			echo "- Plank: Não Instalado"
-			install+="plank "
-		else
-			echo "- Plank: OK"
-	fi
-
-	echo -e "\n=== UTILITÁRIOS ==="
-	## XFCE4 Settings
-	if [ -z "$(command -v xfsettingsd)" ]; then
-			echo "- XFCE Settings: Não Instalado"
-			install+="xfce4-settings "
-		else
-			echo "- XFCE Settings: OK"
-	fi
-
-	## XFCE4 Power Manager
-	if [ -z "$(command -v xfce4-power-manager)" ]; then
-			echo "- XFCE Power Manager: Não Instalado"
-			install+="xfce4-power-manager "
-		else
-			echo "- XFCE Power Manager: OK"
-	fi
-
-	echo -e "\n=== DEPENDÊNCIAS DE SCRIPTS ==="
-	## PyWal
-	if [ -z "$(command -v wal)" ]; then
-			echo "- PyWal: Não Instalado"
-			install+="python-pywal "
-		else
-			echo "- PyWal: OK"
-	fi
-
-	## Maim
-	if [ -z "$(command -v maim)" ]; then
-			echo "- Maim: Não Instalado"
-			install+="maim "
-		else
-			echo "- Maim: OK"
-	fi
-
-	## XClip
-	if [ -z "$(command -v xclip)" ]; then
-			echo "- Xclip: Não Instalado"
-			install+="xclip "
-		else
-			echo "- Xclip: OK"
-	fi
-
-	## XClip
-	if [ -z "$(command -v slop)" ]; then
-			echo "- Slop: Não Instalado"
-			install+="slop "
-		else
-			echo "- Slop: OK"
-	fi
-
-	## XDG Users Dirs
-	if [ -z "$(command -v xdg-user-dir)" ]; then
-			echo "- XDG Users Dirs: Não Instalado"
-			install+="xdg-user-dirs "
-		else
-			echo "- XDG Users Dirs: OK"
-	fi
-
-	## FFMPEG
-	if [ -z "$(command -v ffmpeg)" ]; then
-			echo "- FFMPEG: Não Instalado"
-			install+="ffmpeg "
-		else
-			echo "- FFMPEG: OK"
-	fi
-
-	## ACPI
-	if [ -z "$(command -v acpi)" ]; then
-			echo "- ACPI: Não Instalado"
-			install+="acpi "
-		else
-			echo "- ACPI: OK"
-	fi
-
-	echo -e "\n=== APLICATIVOS BÁSICOS ==="
-
-	## Thunar
-	if [ -z "$(command -v thunar)" ]; then
-			echo "- Thunar: Não Instalado"
-			install+="thunar "
-		else
-			echo "- Thunar: OK"
-	fi
-
-	## Alacritty
-	if [ -z "$(command -v alacritty)" ]; then
-			echo "- Alacritty: Não Instalado"
-			install+="alacritty "
-		else
-			echo "- Alacritty: OK"
-	fi
-
-	## Geany
-	if [ -z "$(command -v geany)" ]; then
-			echo "- Geany: Não Instalado"
-			install+="geany "
-		else
-			echo "- Geany: OK"
-	fi
-
-	##  Pulse Audio Volume Control
-	if [ -z "$(command -v pavucontrol)" ]; then
-			echo "- Pulse Audio Volume Control: Não Instalado"
-			install+="pavucontrol "
-		else
-			echo "- Pulse Audio Volume Control: OK"
-	fi
-
-	##  Viewnior
-	if [ -z "$(command -v viewnior)" ]; then
-			echo "- Viewnior: Não Instalado"
-			install+="viewnior "
-		else
-			echo "- Viewnior: OK"
-	fi
-
-	echo -e "\n=== APPLETS ==="
-	## Network Manager Applet
-	if [ -z "$(command -v nm-applet)" ]; then
-			echo "- Network Manager Applet: Não Instalado"
-			install+="network-manager-applet "
-		else
-			echo "- Network Manager Applet: OK"
-	fi
-
-	## Blueman Applet
-	if [ -z "$(command -v blueman-applet)" ]; then
-			echo "- Blueman Applet: Não Instalado"
-			install+="blueman "
-		else
-			echo "- Blueman Applet: OK"
-	fi
-}
-
-instalarDependenciasAUR () {
-	echo -e "\n## Instalando as Dependências do AUR..."
-	yay $install --answerdiff=None --noconfirm
-}
-
-instalarDependencias () {
-	if [[ $install == "" ]]; then
-		echo "" >> /dev/null
-	else
-		echo "## Instalando as Dependências Faltantes..."
-		sudo pacman -Syu $install --noconfirm
-	fi
+instalarPacotes () {
+	echo -e "\n## Instalando Pacotes..."
+	sleep 1
+	sudo pacman -Syu openbox xorg obconf-qt archlinux-xdg-menu polybar rofi libnotify dunst nitrogen picom xcompmgr plank xfce4-settings xfce4-power-manager python-pywal maim xclip slop xdg-user-dirs ffmpeg acpi thunar alacritty geany pavucontrol viewnior network-manager-applet blueman --noconfirm
 }
 
 instalarFontes () {
-	echo -e "\nPara o Nippybox, são utilizadas algumas fontes para a Interface. E elas serão instaladas globalmente em /usr/share/fonts.\n\n## Copiando as Fontes..."
+	echo -e "\nPara o funcionamento correto do Nippybox,algumas fontes precisam serem instaladas, e elas serão instaladas em /usr/share/fonts, para estarem disponíveis para todos os usuários\n\n## Instalando as Fontes..."
 	sudo cp fonts/* /usr/share/fonts
 	sudo fc-cache -f
 	cd ..
 }
 
 copiarConfigs () {
-	echo -e "\n## Copiando as Configurações..."
+	echo -e "## Copiando as Configurações..."
 	cp -r $OndeEstou/config/* $HOME/.config/
 	cd ..
 
@@ -243,7 +39,7 @@ finalizarConfig () {
 	xdg-user-dirs-update
 
 	echo "## Aplicando o Esquema de Cores e o Papel de Parede"
-	nippy-colorizer $HOME/.config/openbox/wallpaper.jpg
+	bash $HOME/.local/bin/nippy-colorizer $HOME/.config/openbox/wallpaper.jpg
 
 {
 	cat <<EOF
@@ -260,11 +56,10 @@ creditos () {
 
 }
 
-echo -e "Bem-vindo ao instalador do Nippybox!\nO Nippybox é uma personalização do Openbox que acabei criando e que atende às minhas necessidades\n\nEventualmente o Script irá pedir a senha do super-usuário (root) para instalar alguns pacotes faltantes e as fontes, mas não se preocupe."
+echo -e "Bem-vindo ao instalador do Nippybox!\nO Nippybox é uma personalização do Openbox que acabei fazendo e que atende às minhas necessidades\n\nEventualmente o Script irá pedir a senha do super-usuário (root) para instalar alguns pacotes faltantes e as fontes, mas não se preocupe."
 verificarDiretorios
 sleep 1
-verificarDependencias
-instalarDependencias
+instalarPacotes
 instalarFontes
 copiarConfigs
 finalizarConfig
