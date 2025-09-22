5c5da2e9a89be9276363753521fc9e61c089f8be
Guilherme de Oliveira <coelhoposa@proton.me>
2025-09-21 22:20:03 -0300

    Organização do Código
    O Código foi organizado para que as funções fiquem na ordem certa e foi retirada a parte da instalação do YAY. O Yay agora é instalado apenas pelo ChaoticAUR.

--- install.sh
+++ install.sh
@@ -11,7 +11,7 @@ LightDMBack="Oranges by tamanna_rumee.png"
 verificarDiretorios () {
 	echo -e "\n## Verificando Diretórios..."
 	mkdir -p $HOME/.local/bin
-	mkdir -p $HOME/.local/share/plank/themes
+	mkdir -p $HOME/.local/share/plank/themes/Nippy
 	mkdir -p $HOME/.config
 	mkdir -p $HOME/.themes/nippybox
 	mkdir -p $HOME/.config/xfce4/xfconf/xfce-perchannel-xml/
@@ -21,55 +21,6 @@ instalarPacotes () {
 	echo -e "\n## Instalando Pacotes Básicos..."
 	sleep 1
 	sudo pacman -Syu nano fastfetch openbox xorg obconf-qt archlinux-xdg-menu polybar rofi libnotify dunst nitrogen picom xcompmgr plank xfce4-settings xfce4-power-manager python-pywal maim xclip slop xdg-user-dirs ffmpeg acpi thunar alacritty geany pavucontrol viewnior network-manager-applet blueman gvfs xfce4-terminal pulsemixer xorg-xbacklight pulseaudio pulseaudio-bluetooth pulseaudio-alsa playerctl clipnotify noto-fonts-emoji bash-completion mate-system-monitor brightnessctl system-config-printer bluez-utils redshift curl qt5ct qt6ct --noconfirm --needed
-}
-
-instalarFontes () {
-	echo -e "\n## Instalando as Fontes..."
-	sudo cp fonts/* /usr/share/fonts
-	sudo fc-cache -f
-	cd $ondeEstou
-}
-
-copiarConfigs () {
-	echo -e "## Copiando as Configurações..."
-	cp -r $OndeEstou/config/* $HOME/.config/
-
-	echo "## Copiando Temas..."
-	cp -r $OndeEstou/themes/* $HOME/.themes/
-
-	echo "## Copiando Scripts..."
-	cp -r $OndeEstou/scripts/* $HOME/.local/bin/
-	chmod +x $HOME/.local/bin/*
-}
-
-finalizarConfig () {
-	echo "## Gerando as pastas do Usuário"
-	xdg-user-dirs-update
-
-	echo "## Aplicando o Esquema de Cores"
-	bash $HOME/.local/bin/nippy-colorizer "/usr/share/backgrounds/Tongue Cat by Nennieinszweidrei.png" --no-X11
-	
-	echo "## Aplicando Temas"
-	xfconf-query -c xsettings -p /Net/ThemeName -s "Dracula"
-	xfconf-query -c xsettings -p /Net/IconThemeName -s "Papirus-Dark"
-	xfconf-query -c xsettings -p /Gtk/FontName -s "Cantarell 9"
-
-	echo "## Configurando a Doca"
-	dconf write /net/launchpad/plank/docks/dock1/zoom-enabled true
-	dconf write /net/launchpad/plank/docks/dock1/theme "'Nippy'"
-	
-
-	echo "## Gerando o .xinitrc..."
-
-	{
-		cat <<EOF
-#!/bin/bash
-
-XDG_SESSION_TYPE=x11
-exec openbox-session
-
-EOF
-	} > $HOME/.xinitrc
 }
 
 instalarExtras () {
@@ -118,7 +69,69 @@ instalarExtras () {
 
 	echo "## Corrigindo o Thunar..."
 	sudo nippy-hooks fix-thunar
+}
 
+instalarFontes () {
+	echo -e "\n## Instalando as Fontes..."
+	sudo cp fonts/* /usr/share/fonts
+	sudo fc-cache -f
+	cd $ondeEstou
+}
+
+copiarConfigs () {
+	echo -e "## Copiando as Configurações..."
+	cp -r $OndeEstou/config/* $HOME/.config/
+
+	echo "## Copiando Temas..."
+	cp -r $OndeEstou/themes/* $HOME/.themes/
+
+	echo "## Copiando Scripts..."
+	cp -r $OndeEstou/scripts/* $HOME/.local/bin/
+	chmod +x $HOME/.local/bin/*
+}
+
+chaoticAUR () {
+	sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
+	sudo pacman-key --lsign-key 3056513887B78AEB
+	
+	sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' --noconfirm --needed
+	sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst' --noconfirm --needed
+	
+	sudo sed -i '$a\[chaotic-aur]\nInclude = /etc/pacman.d/chaotic-mirrorlist' /etc/pacman.conf
+		
+	sudo pacman -Syu yay betterlockscreen dracula-gtk-theme --noconfirm --needed
+	
+	yay -S dracula-gtk-theme --noconfirm
+}
+
+finalizarConfig () {
+	echo "## Gerando as pastas do Usuário"
+	xdg-user-dirs-update
+
+	echo "## Aplicando o Esquema de Cores"
+	bash $HOME/.local/bin/nippy-colorizer "/usr/share/backgrounds/Tongue Cat by Nennieinszweidrei.png" --no-X11
+	
+	echo "## Aplicando Temas"
+	xfconf-query -c xsettings -p /Net/ThemeName -s "Dracula"
+	xfconf-query -c xsettings -p /Net/IconThemeName -s "Papirus-Dark"
+	
+	echo "## Aplicando Fonte"
+	xfconf-query -c xsettings -p /Gtk/FontName -s "Cantarell 9"
+
+	echo "## Configurando a Doca"
+	dconf write /net/launchpad/plank/docks/dock1/zoom-enabled true
+	dconf write /net/launchpad/plank/docks/dock1/theme "'Nippy'"
+
+	echo "## Gerando o .xinitrc..."
+	{
+		cat <<EOF
+#!/bin/bash
+
+XDG_SESSION_TYPE=x11
+exec openbox-session
+
+EOF
+	} > $HOME/.xinitrc
 }
 
 temaPlank () {
@@ -135,52 +148,11 @@ FillEndColor=0;;0;;0;;217
 InnerStrokeColor=255;;255;;255;;255
 
 EOF
-	} > $HOME/.local/share/plank/themes/hover.theme
-	
-}
-
-chaoticAUR () {
-	sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
-	sudo pacman-key --lsign-key 3056513887B78AEB
-	
-	sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' --noconfirm --needed
-	sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst' --noconfirm --needed
-	sudo sed -i '$a\[chaotic-aur]\nInclude = /etc/pacman.d/chaotic-mirrorlist' /etc/pacman.conf
-		
-	sudo pacman -Syu yay betterlockscreen --noconfirm --needed
-	yay -S dracula-gtk-theme --noconfirm
-}
-
-instalarAUR () {
-	cd /tmp
-	echo -e "\nHá alguns pacotes que compõe o Nippybox que estão no AUR (Arch Linux User Repository), e para instalar eles é necessário o uso de um ajudante de AUR. O yay é o Ajudante de AUR que esse Script utiliza."
-	sleep 1
-
-	echo -e "\n## Instalando as Dependências do Yay..."
-	sudo pacman -S git go make --noconfirm
-
-	echo -e "\n## Clonando o Repositório do Yay..."
-	git clone https://aur.archlinux.org/yay.git
-	cd yay
-
-	echo -e "\n## Construindo e Instalando o Yay..."
-	makepkg -si --noconfirm --needed
-
-	echo -e "\n## Removendo o Pacote Go, usado na Construção do Yay..."
-	sudo pacman -Rs go --noconfirm
-
-	echo -e "\n## Gerando Banco de Dados do Yay..."
-	yay -Y –gendb
-
-	echo "## Instalando os Pacotes do AUR..."
-	yay -S betterlockscreen dracula-gtk-theme --noconfirm
-
-	cd $OndeEstou
+	} > $HOME/.local/share/plank/themes/Nippy/hover.theme
 }
 
 creditos () {
 	echo -e "\nCréditos ao Aditya Shakya, que foi o responsável pelas personalizações do Rofi, da Polybar e de alguns dos Scripts que foram implementados no Nippybox"
-
 }
 
 echo -e "\nBem-vindo ao instalador do Nippybox!\nO Nippybox é uma personalização do Openbox com o objetivo de ser simples de usar em que juntei algumas coisas legais por aí e que me agradaram."
@@ -191,7 +163,6 @@ instalarPacotes
 instalarExtras
 instalarFontes
 copiarConfigs
-#instalarAUR
 chaoticAUR
 finalizarConfig
 temaPlank
